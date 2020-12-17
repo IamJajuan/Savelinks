c80b736348be570dfafdcf247c1aa3d8085f1867
Jajuan Hall <70973703+kingfishergod@users.noreply.github.com>
2020-12-16 19:36:43 -0500

    Add files via upload

--- front-end/reducers/index.js
+++ front-end/reducers/index.js
@@ -3,12 +3,20 @@ import { authReducer } from "./AuthReducer";
 import { linkReducer } from './LinkReducer';
 import { categoryReducer } from "./CategoryReducer";
 import { NextReducer } from './NextReducer';
+import persistReducer from 'redux-persist/es/persistReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+const linksPersistConfig = {
+    key: 'links',
+    storage: AsyncStorage,
+    blacklist: ['error','link']
+  }
 export default combineReducers(
     {
         auths: authReducer,
         categories:categoryReducer,
-        links:linkReducer,
+        links:persistReducer(linksPersistConfig,linkReducer),
         nexts:NextReducer
         
        
